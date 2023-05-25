d53f628edff1521e4720f52ccbf6dbc8e68a7b8c
James Wang <james_wangx@163.com>
2023-05-25 17:35:28 +0800

    Add log processing

--- src/main/java/com/codicefun/blog/web/IndexController.java
+++ src/main/java/com/codicefun/blog/web/IndexController.java
@@ -1,19 +1,15 @@
 package com.codicefun.blog.web;
 
-import com.codicefun.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        // int i = 9 / 0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable String id, @PathVariable String name) {
+        System.out.println(id + ", " + name);
 
         return "index";
     }
