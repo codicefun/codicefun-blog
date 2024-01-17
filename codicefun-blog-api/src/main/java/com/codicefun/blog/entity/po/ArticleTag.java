50db4253d01d876544631b4821872cc7e7657e90
James Wang <james_wangx@163.com>
2024-01-17 12:14:58 +0800

    Add generate po ArticleTag

---
+++ codicefun-blog-api/src/main/java/com/codicefun/blog/entity/po/ArticleTag.java
@@ -0,0 +1,17 @@
+package com.codicefun.blog.entity.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+public class ArticleTag {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    private Integer articleId;
+
+    private Integer tagId;
+
+}
