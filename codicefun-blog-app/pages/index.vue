1e0661ba7fbf3d209d04b97a3c01d67be56dd900
James Wang <james_wangx@163.com>
2024-01-18 19:11:28 +0800

    Update refactoring frontend use nuxt

---
+++ codicefun-blog-app/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const data = ref({})
+
+const getUserList = async () => {
+  data.value = await useFetch('/user', { baseURL: 'http://localhost:8080/api' })
+  console.log(data.value)
+}
+</script>
+
+<template>
+  <h1>Welcome to the homepage</h1>
+  <button @click="getUserList">Click</button>
+  <p>{{ data }}</p>
+</template>
+
+<style scoped>
+
+</style>
