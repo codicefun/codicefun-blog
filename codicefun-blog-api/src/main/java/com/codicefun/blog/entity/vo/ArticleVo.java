72e7af87929e26a1b304bb1695f3d3ea6d5a92bc
James Wang <james_wangx@163.com>
2024-01-25 21:08:58 +0800

    fix: remove method in vo

--- codicefun-blog-api/src/main/java/com/codicefun/blog/entity/vo/ArticleVo.java
+++ codicefun-blog-api/src/main/java/com/codicefun/blog/entity/vo/ArticleVo.java
@@ -1,10 +1,6 @@
 package com.codicefun.blog.entity.vo;
 
-import com.codicefun.blog.entity.po.Article;
-import com.codicefun.blog.entity.po.Type;
-import com.codicefun.blog.entity.po.User;
 import lombok.Data;
-import org.springframework.beans.BeanUtils;
 
 import java.util.Date;
 
@@ -23,24 +19,5 @@ public class ArticleVo {
 
     private String username;
     private String typeName;
-
-    // TODO: Remove entity convert method
-    public static ArticleVo po2vo(Article article) {
-        ArticleVo articleVO = new ArticleVo();
-        BeanUtils.copyProperties(article, articleVO);
-        articleVO.username = article.getUser().getUsername();
-        articleVO.typeName = article.getType().getName();
-
-        return articleVO;
-    }
-
-    public static Article vo2po(ArticleVo articleVO) {
-        Article article = new Article();
-        BeanUtils.copyProperties(articleVO, article);
-        article.setUser(User.of(articleVO.username));
-        article.setType(Type.of(articleVO.typeName));
-
-        return article;
-    }
 
 }
