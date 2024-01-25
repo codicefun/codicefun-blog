542d8d5f1c8a6addea153daaafca37cd51053df5
James Wang <james_wangx@163.com>
2024-01-25 15:51:16 +0800

    fix: add validate api

--- codicefun-blog-api/src/main/java/com/codicefun/blog/controller/AuthController.java
+++ codicefun-blog-api/src/main/java/com/codicefun/blog/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.codicefun.blog.controller;
 
+import com.codicefun.blog.entity.enums.ResponseStatusEnum;
 import com.codicefun.blog.entity.po.User;
 import com.codicefun.blog.entity.vo.ResponseVO;
 import com.codicefun.blog.entity.vo.TokenVO;
+import com.codicefun.blog.exception.BusinessException;
 import com.codicefun.blog.service.UserService;
+import com.codicefun.blog.util.JwtUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
@@ -26,5 +31,15 @@ public class AuthController {
 
         return ResponseVO.success(tokenVO);
     }
+
+    @PostMapping("/validate")
+    public ResponseVO<Object> validate(@RequestBody TokenVO tokenVO) {
+        if (jwtUtil.validateToken(tokenVO.getToken())) {
+            return ResponseVO.success();
+        } else {
+            throw new BusinessException(ResponseStatusEnum.INVALID_TOKEN);
+        }
+    }
+
 
 }
